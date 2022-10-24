d3ec162b2cc34d88deed0cec1baee177f2452ade
carlos <carlos.rueda@itglobers.com>
2022-10-24 17:26:30 -0500

    lazy load basico listo cambios

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -15,7 +15,7 @@ const lazy1 = lazy(
 );
 const lazy2 = lazy(
   () =>
-    import(/*webpackChunkName: "LazyPage2*/ "../01-lazyload/pages/LazyPage2")
+    import(/*webpackChunkName: "LazyPage2"*/ "../01-lazyload/pages/LazyPage2")
 );
 const lazy3 = lazy(
   () =>
